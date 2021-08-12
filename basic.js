900f4037f7b2e0e6185a671812aeba57c3ef6cc0
Tony <un_razum@mail.ru>
2021-08-12 21:44:00 +0300

    class concat word

--- basic.js
+++ basic.js
@@ -131,33 +131,53 @@ console.log(a); // 7
 
 // 'use strict'
 
-(function () {
-  "use strict";
+// (function () {
+//   "use strict";
 
-  for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-  }
-})();
+//   for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//   }
+// })();
 
-/// https://svgjs.dev/docs/3.0/
-var input = document.querySelector("input[type=text]");
-var draw = SVG().addTo("#drawing").viewbox(0, 0, 300, 140);
-var text = draw.text(function (add) {
-  add.tspan(input.value);
-});
+// /// https://svgjs.dev/docs/3.0/
+// var input = document.querySelector("input[type=text]");
+// var draw = SVG().addTo("#drawing").viewbox(0, 0, 300, 140);
+// var text = draw.text(function (add) {
+//   add.tspan(input.value);
+// });
 
-textPath = text.path("M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80");
+// textPath = text.path("M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80");
 
-textPath
-  .animate(1000)
-  .ease("<>")
-  .plot("M10 80 C 40 150, 65 150, 95 80 S 150 10, 180 80")
-  .loop(true, true);
+// textPath
+//   .animate(1000)
+//   .ease("<>")
+//   .plot("M10 80 C 40 150, 65 150, 95 80 S 150 10, 180 80")
+//   .loop(true, true);
 
-input.addEventListener("keyup", updateText(textPath));
+// input.addEventListener("keyup", updateText(textPath));
 
 function updateText(textPath) {
   return function () {
     textPath.tspan(this.value);
   };
 }
+
+// class
+class concatWord {
+  constructor(...args) {
+    this.args = args;
+  }
+  consoleLog() {
+    let container = "";
+    for (let word of this.args) {
+      container = container + "^" + word;
+      console.log(container);
+    }
+    console.log(container);
+  }
+}
+
+let one = new concatWord("one", "two", "sdfl");
+let two = new concatWord("sdhf", "dfsd");
+one.consoleLog();
+two.consoleLog();
